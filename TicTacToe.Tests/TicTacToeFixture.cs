809afb152c503368a523daf22e44c0791fc85fb0
Bernd Scholze <bscholze@gmail.com>
2020-07-15 21:03:35 -0400

    Initial setup of empty projects

---
+++ TicTacToe.Tests/TicTacToeFixture.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class TicTacToeFixture
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This will be the TicTacToe board game");
+        }
+    }
+}
